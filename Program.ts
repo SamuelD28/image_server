743c38800b85755576facda72e833d7e2044f8a5
samuel dubé <Samuel_personnel@outlook.com>
2019-10-21 17:17:50 -0400

    Add : Cors support

--- Program.ts
+++ Program.ts
@@ -1,16 +1,14 @@
 /**
  * Entry point for the image server
  *
- * TODO : Error Handler quand le champ des images est pas present.
- * TODO  : Error handler pour les erreurs trop large
  */
 
 import express from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
-import path from "path";
 import { Mongodb } from "./modules/database/Index";
 import ImageController from "./modules/image_uploader/ImageController";
+import cors from "cors";
 
 // Parse the .env of the project in the process runtime
 dotenv.config();
@@ -18,6 +16,8 @@ dotenv.config();
 const App = express();
 const Port: number = process.env.PORT ? +process.env.PORT : 8080;
 const Database: Mongodb = new Mongodb(<string>process.env.MONGODB_URI, <string>process.env.MONGODB_NAME);
+
+App.use(cors());
 
 App.use(bodyParser.urlencoded({ extended: true }));
 
